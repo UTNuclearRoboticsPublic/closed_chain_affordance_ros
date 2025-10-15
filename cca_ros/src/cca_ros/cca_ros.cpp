22822793d5ba19ee166ffd6a040e2bf138867912
cjans121 <jpanthi@utexas.edu>
2025-10-15 17:09:30 -0500

    enable execution trajectory checking

--- cca_ros/src/cca_ros/cca_ros.cpp
+++ cca_ros/src/cca_ros/cca_ros.cpp
@@ -811,7 +811,7 @@ bool CcaRos::execute_trajectory_(rclcpp_action::Client<FollowJointTrajectory>::S
         const KinematicState current_state = read_joint_states_();
         const Eigen::VectorXd goal_state =
             Eigen::VectorXd::Map(goal.trajectory.points[0].positions.data(), current_state.robot.size());
-        const double tolerance = 3 * 1e-1; // Declare tolerance as double
+        const double tolerance = 1 * 1e-1; // Declare tolerance as double
 
         // Compare goal state and current state within the tolerance
         if ((goal_state - current_state.robot).cwiseAbs().maxCoeff() > tolerance)
@@ -833,8 +833,8 @@ bool CcaRos::execute_trajectory_(rclcpp_action::Client<FollowJointTrajectory>::S
             RCLCPP_ERROR(node_logger_, "Current State: %s", current_state_stream.str().c_str());
             RCLCPP_ERROR(node_logger_, "Trajectory Start State: %s", goal_state_stream.str().c_str());
 
-            // *status_ = Status::FAILED;
-            // return false;
+            *status_ = Status::FAILED;
+            return false;
         }
     }
     catch (const std::runtime_error &e)
