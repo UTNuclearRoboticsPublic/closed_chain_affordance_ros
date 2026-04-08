25807cf57bb0cae99cecef5636827719a9e28903
cjans121 <jpanthi@utexas.edu>
2026-04-08 17:53:56 -0500

    add missing cca_ros_context_ member

--- cca_ros_behavior/include/cca_ros_behavior/cca_ros_behavior.hpp
+++ cca_ros_behavior/include/cca_ros_behavior/cca_ros_behavior.hpp
@@ -68,6 +68,7 @@ class CcaRosAction : public BT::StatefulActionNode
 
   private:
     std::shared_ptr<cca_ros::CcaRos> cca_ros_;                         /**< Node for ROS communication */
+    std::shared_ptr<cca_ros::CcaRosContext> cca_ros_context_;                         /**< Node for ROS communication */
     std::jthread spinner_thread_;                                    /**< Thread to spin the node. */
     std::shared_ptr<cca_ros::Status> status_{nullptr};              /**< To check the status of the CCA action. */
     std::chrono::time_point<std::chrono::steady_clock> start_time_; /**< To monitor the timeout. */
